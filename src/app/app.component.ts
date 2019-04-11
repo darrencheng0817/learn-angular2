9581364d350d6562a2503ba4c474191bfa0f1dec
Darren <darrencheng0817@gmail.com>
2019-04-11 22:04:52 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit{
+  globalTopPerformings = [['US', 88], ['UK', 85], ['China', 76]];
+  data = {
+    football: {
+      allCountries : [
+        ['Country', 'performance'],
+        ['US', 87],
+        ['UK', 84]
+      ],
+      best : [['AAA', 88], ['BBB', 85], ['CCC', 76]],
+      worst : [['CCC', 88], ['BBB', 85], ['AAA', 76]]
+    }
+  };
+  constructor() { }
+
+  ngOnInit() {
+    //init map here
+    console.log("init map");
+  }
+
+  handleInput(keyword) {
+    //update map here
+    console.log(keyword);
+  }
 }
