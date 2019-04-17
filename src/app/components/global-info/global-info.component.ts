6274e10eb036ea4ebeaaee32bb4c05f7ab250e8d
Darren <darrencheng0817@gmail.com>
2019-04-18 01:02:32 +0800

    global info component

--- src/app/components/global-info/global-info.component.ts
+++ src/app/components/global-info/global-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-global-info',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalInfoComponent implements OnInit {
 
+  @Input() globalTopPerformances: Array<any>;
   constructor() { }
 
   ngOnInit() {
